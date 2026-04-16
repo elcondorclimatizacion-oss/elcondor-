8a10e810a8569abab7e89126e52206a1898753ea
elcondorclimatizacion-oss <elcondorclimatizacion@gmail.com>
2026-04-15 23:52:09 -0300

    Add files via upload

---
+++ SUBIR_A_GITHUB.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Subiendo cambios a GitHub...
+git add .
+git commit -m "Actualizacion herramientas El Condor y QR Publicitario"
+git push
+echo.
+echo ==========================================
+echo ¡PROCESO COMPLETADO! 🦅
+echo Tus archivos ya deberian estar en GitHub.
+echo ==========================================
+pause
